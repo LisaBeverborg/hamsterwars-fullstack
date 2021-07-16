9b6f217f9742c509c45182f9f254ce6c5caab1c1
lisa.beverborg <lisa.beverborg@gmail.com>
2021-07-16 20:23:47 +0200

    Header update

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import styled from "styled-components";
 
 const Start = () => {
 
 
         return (
             <div>
-                <h2>Welcome to Hamsterwars</h2>
-                <p> Set up hamsters for Battle. In the end only <em>one</em> hamster can win the title <strong>Cutest Craziest Hamster</strong></p>
+                <Title>Welcome to Hamsterwars</Title>
+                <Text> Set up hamsters for Battle. In the end only <em>one</em> hamster can win the title <strong>Cutest Craziest Hamster</strong>. <br></br>
+                You can have look at all Hamsters in the gallery and let them battle for the title cutest Hamster.</Text>
+                <Text>This project is part of the course Effective Frontend Development which is part of the education Frontend Development 
+                at the IT University in Gothenburg / Stockholm. <br></br>In the project, frontend and backend have been combined with React on 
+                the frontend side and Node.js, Express and cloud database Google Firestore on the backend side.</Text>
             </div>
 
 
         )
     }
 
+    const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+  `;
 
+  const Text = styled.p`
+  font-size: 1em;
+  text-align: center;
+`;
 export default Start;
